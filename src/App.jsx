cbb2ae44520970eebcf1f528a98082872f99d0ed
Sebastian Galustyan <sebastian@7digit.io>
2022-09-26 03:01:48 +0100

    Create: initial set up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import USupport from "./assets/USupport.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src={USupport} className="logo react" alt="React logo" />
+      </div>
+      <p>Website Interface</p>
+    </div>
+  );
+}
+
+export default App;
