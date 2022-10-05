706940df37bbe9104fca01a3a9ad9baf87d8419a
Vasilen Tsvetkov <vasilen@7digit.io>
2022-10-05 23:53:07 +0100

    Refactor: Hero Block to use Grid and GridItem components

--- src/blocks/Hero/Hero.jsx
+++ src/blocks/Hero/Hero.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Block } from "/USupport-components-library/src";
-import { StoreButton } from "/USupport-components-library/src";
+import {
+  Block,
+  Grid,
+  GridItem,
+  StoreButton,
+} from "/USupport-components-library/src";
+
+import "./hero.scss";
 
 import MascotOrange from "../../assets/MascotOrange.png";
-import "./hero.scss";
 
 /**
  * Hero
@@ -12,32 +17,40 @@ import "./hero.scss";
  *
  * @return {jsx}
  */
-export function Hero() {
-  return (
-    <Block classes={["hero"]}>
-      <div className="hero__content">
-        <div className="hero__information-container">
-          <h3 className="hero__heading">
-            Make your mental health <span>a priority!</span>
-          </h3>
-          <p className="small-text hero__description">
-            USupport is the first platform that provides you with personalized
-            care for your mental health and personal development and provides
-            you with easy access to resources and proven specialists. Start by
-            telling your story!
-          </p>
-          <div className="hero__buttons-container">
-            <StoreButton size="lg" store="google-play" />
-            <StoreButton size="lg" store="app-store" />
-          </div>
-        </div>
 
-        <div className="hero__mascot-container">
-          <img src={MascotOrange} className="hero__mascot" />
-        </div>
-      </div>
+export const Hero = () => {
+  return (
+    <Block classes="hero">
+      <Grid classes="hero__main-grid">
+        <GridItem md={5} lg={6}>
+          <Grid classes="hero__content-grid">
+            <GridItem md={8} lg={12} classes="hero__heading-item">
+              <h1>
+                Make your mental health <span>a priority!</span>
+              </h1>
+            </GridItem>
+            <GridItem md={8} lg={12} classes="hero__text-item">
+              <p className="paragraph">
+                USupport is the first platform that provides you with
+                personalized care for your mental health and personal
+                development and provides you with easy access to resources and
+                proven specialists. Start by telling your story!
+              </p>
+            </GridItem>
+            <GridItem md={4} lg={6} classes="hero__buttons-item">
+              <StoreButton size="lg" store="google-play" />
+            </GridItem>
+            <GridItem md={4} lg={6} classes="hero__buttons-item">
+              <StoreButton size="lg" store="app-store" />
+            </GridItem>
+          </Grid>
+        </GridItem>
+        <GridItem md={3} lg={6} classes="hero__mascot-item">
+          <img src={MascotOrange} />
+        </GridItem>
+      </Grid>
       <div className="radial-gradient-purple" />
       <div className="radial-gradient-blue" />
     </Block>
   );
-}
+};
