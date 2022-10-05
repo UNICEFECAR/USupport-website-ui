b4402ccf5247601b8dd0826d3b2415579716674e
Vasilen Tsvetkov <vasilen@7digit.io>
2022-10-05 22:11:05 +0100

    Add: addons to storybook for custom breakpoints and viewports

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,3 +1,34 @@
+const customViewports = {
+  sm: {
+    name: 'Small',
+    styles: {
+      width: '375px',
+      height: '963px',
+    },
+  },
+  md: {
+    name: 'Medium',
+    styles: {
+      width: '768px',
+      height: '801px',
+    },
+  },
+  lg: {
+    name: 'Large',
+    styles: {
+      width: '1366px',
+      height: '801px',
+    },
+  },
+  xl: {
+    name: 'Extra Large',
+    styles: {
+      width: '1600px',
+      height: '801px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +36,14 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  viewport: { viewports: customViewports },
+  breakpoints: {
+    breakpointNames: {
+      'sm': '375',
+      'md': '768',
+      'lg': '1366',
+      'xl': '1600'
+    }
   },
 }
