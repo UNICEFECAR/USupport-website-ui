a5340a723be164744bf028b76465362a461594ae
Vasilen Tsvetkov <vasilen@7digit.io>
2022-10-05 22:15:37 +0100

    Create: HowItWorks block

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hero } from "../../blocks/Hero";
+import { HowItWorks } from "../../blocks/HowItWorks";
 
 export function Landing() {
   return (
     <React.Fragment>
       <Hero />
+      <HowItWorks />
     </React.Fragment>
   );
 }
