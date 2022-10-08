f1b96e74f19d27e2cea4bcb8f0856dd5d52e50e1
Vasilen Tsvetkov <vasilen@7digit.io>
2022-10-08 21:45:38 +0100

    Refactor: reorder the elements within the Landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,14 +3,18 @@ import { Hero } from "../../blocks/Hero";
 import { FindYourself } from "../../blocks/FindYourself";
 import { HowItWorks } from "../../blocks/HowItWorks";
 import { About } from "../../blocks/About";
+import { FAQ } from "../../blocks/FAQ";
+import { DownloadApp } from "../../blocks/DownloadApp";
 
 export function Landing() {
   return (
     <React.Fragment>
       <Hero />
       <FindYourself />
-      <HowItWorks />
       <About />
+      <HowItWorks />
+      <FAQ />
+      <DownloadApp />
     </React.Fragment>
   );
 }
