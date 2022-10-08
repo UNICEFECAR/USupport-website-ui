c80722d075406e5bfe513d3a4ede1be00298d073
Vasilen Tsvetkov <vasilen@7digit.io>
2022-10-08 23:29:24 +0100

    Create: NotFound page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Page } from "../../blocks/Page/Page";
+
+import "./not-found.scss";
+
+/**
+ * NotFound page.
+ *
+ * NotFound
+ *
+ * @returns {JSX.Element}
+ */
+export const NotFound = () => {
+  return <Page>NotFound Page</Page>;
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export * from "./Landing";
 export * from './SOSCenter';
+export * from './NotFound';
