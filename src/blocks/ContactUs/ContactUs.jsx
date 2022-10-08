aa719df5db6d0f26bfc37cd1bd3747d5a2137ac9
Vasilen Tsvetkov <vasilen@7digit.io>
2022-10-08 19:10:45 +0100

    Refactor: align ContactUs block to the style guidelines

--- src/blocks/ContactUs/ContactUs.jsx
+++ src/blocks/ContactUs/ContactUs.jsx
@@ -5,10 +5,11 @@ import {
   Grid,
   GridItem,
 } from "/USupport-components-library/src";
-
 import { useWindowDimensions } from "/USupport-components-library/src/utils";
-import mascot from "../../assets/MascotBlue.png";
+
 import "./contact-us.scss";
+
+import mascot from "../../assets/MascotBlue.png";
 
 /**
  * ContactUs
@@ -20,25 +21,24 @@ import "./contact-us.scss";
 export const ContactUs = () => {
   const { width } = useWindowDimensions();
   const showMascot = width >= 768;
+
   return (
     <Block classes="contact-us">
       <Grid classes="contact-us__main-grid">
-        <GridItem md={8} lg={12} classes="contact-us__heading">
+        <GridItem md={8} lg={12} classes="contact-us__heading-item">
           <h2>Contact Us</h2>
         </GridItem>
         <GridItem md={8} lg={12}>
           <Grid classes="contact-us__secondary-grid">
-            <GridItem md={8}>
-              <h4 className="contact-us__subheading">
-                Have a question? Don’t hesitate to contact us.
-              </h4>
+            <GridItem md={8} classes="contact-us__subheading-item">
+              <h4>Have a question? Don’t hesitate to contact us.</h4>
             </GridItem>
             <GridItem classes="contact-us__form-item" md={4} lg={8}>
               <ContactForm />
             </GridItem>
             {showMascot && (
               <GridItem md={4} lg={4} classes="contact-us__mascot-item">
-                <img className="contact-us__mascot-item__image" src={mascot} />
+                <img src={mascot} />
               </GridItem>
             )}
           </Grid>
