34ae6ac4ad86bd3517d5b428288a33a11472bbed
Sebastian Galustyan <sebastian@7digit.io>
2022-10-20 14:45:15 +0100

    Create: cmsService exporting functions which are going to be used for retreving data from CMS

---
+++ src/services/cmsService.js
@@ -0,0 +1,111 @@
+import http from "./httpService";
+import { CMS_API_URL } from "../config/config.json";
+
+const articlesEndpoint = CMS_API_URL + "/articles";
+const ageGroupsEndpoint = CMS_API_URL + "/age-groups";
+const categoriesEndpoint = CMS_API_URL + "/categories";
+
+/**
+ * send request to get all articles data with a predefined limit and filtered by age group, category and search string
+ *
+ * @param {string} limit - the number of articles to return
+ * @param {string} contains - the string to search for in the title
+ * @param {string} ageGroupId - the id of the age group to filter by
+ * @param {string} categoryId - the id of the category to filter by
+ *
+ * @returns {object} the articles data
+ */
+async function getArticlesLimit(limit, contains, ageGroupId, categoryId) {
+  const { data } = await http.get(
+    `${articlesEndpoint}?pagination[limit]=${limit}&populate=*&filters[title][$containsi]=${contains}&filters[age_groups][id][$in]=${ageGroupId}&filters[category][id][$in]=${categoryId}`
+  );
+
+  return data;
+}
+
+/**
+ * send request to get artcicles data starting from a specific index within the articles collection, with predefined limit and filtered by age group, category and search string
+ *
+ * @param {string} start - the index of the first article to return
+ * @param {string} limit - the number of articles to return
+ * @param {string} contains - the string to search for in the title
+ * @param {string} ageGroupId - the id of the age group to filter by
+ * @param {string} categoryId - the id of the category to filter by
+ *
+ * @returns {object} the articles data
+ */
+async function getArticlesStartLimit(
+  start,
+  limit,
+  contains,
+  ageGroupId,
+  categoryId
+) {
+  const { data } = await http.get(
+    `${articlesEndpoint}?pagination[start]=${start}&pagination[limit]=${limit}&populate=*&filters[title][$containsi]=${contains}&filters[age_groups][id][$in]=${ageGroupId}&filters[category][id][$in]=${categoryId}`
+  );
+
+  return data;
+}
+
+/**
+ * send request to get data for a specific article by id
+ *
+ * @param {string} id - the id of the article
+ *
+ * @returns {object} the articles data
+ */
+async function getArticleById(id) {
+  const { data } = await http.get(`${articlesEndpoint}/${id}?populate=*`);
+
+  return data;
+}
+
+/**
+ * send request to get the newest articles
+ *
+ * @param {string} limit - the number of articles to return
+ *
+ * @returns {object} the articles data
+ */
+async function getNewestArticles() {
+  const { data } = await http.get(
+    `${articlesEndpoint}?pagination[limit]=1&sort[0]=createdAt%3Adesc&populate=*`
+  );
+
+  return data;
+}
+
+/**
+ *
+ * send request to get all the categories
+ *
+ * @returns {object} the categories data
+ *
+ */
+async function getCategories() {
+  const { data } = await http.get(`${categoriesEndpoint}`);
+  return data;
+}
+
+/**
+ *
+ * send request to get all ageGroups
+ *
+ * @returns {object} the ageGroups data
+ */
+async function getAgeGroups() {
+  const { data } = await http.get(`${ageGroupsEndpoint}`);
+  return data;
+}
+
+const exportedFunctions = {
+  getArticlesLimit,
+  getArticlesStartLimit,
+  getArticleById,
+  getNewestArticles,
+  getCategories,
+  getAgeGroups,
+};
+
+export default exportedFunctions;
