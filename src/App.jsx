78e1e2033d792b15422b8ff4f2dfa4691bcde9de
Georgi 7DIGIT <georgi@7digit.io>
2022-10-29 18:04:33 +0300

    Add: react-query library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   Landing,
   SOSCenter,
@@ -16,6 +17,9 @@ import "aos/dist/aos.css";
 import AOS from "aos";
 
 import "./App.scss";
+
+// Create a react-query client
+const queryClient = new QueryClient();
 
 function App() {
   // TODO: add the country specific information about the SOS center
@@ -52,18 +56,23 @@ function App() {
   });
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/how-it-works" element={<HowItWorks />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/information-portal" element={<InformationPortal />} />
-        <Route path="/article/:id" element={<ArticleInformation />} />
-        <Route path="/sos-center" element={<SOSCenter contacts={contacts} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/information-portal" element={<InformationPortal />} />
+          <Route path="/article/:id" element={<ArticleInformation />} />
+          <Route
+            path="/sos-center"
+            element={<SOSCenter contacts={contacts} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
