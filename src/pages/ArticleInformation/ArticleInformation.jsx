2439dea67b07e8dc5b2864a269b8311a5d7f39a8
Sebastian Galustyan <sebastian@7digit.io>
2022-11-16 01:59:41 +0000

    Refactor: ArticlesInformation page to fetch articles based on existent array of article ids set by country admins

--- src/pages/ArticleInformation/ArticleInformation.jsx
+++ src/pages/ArticleInformation/ArticleInformation.jsx
@@ -11,7 +11,7 @@ import {
   CardMedia,
   Loading,
 } from "@USupport-components-library/src";
-import { cmsSvc } from "@USupport-components-library/services";
+import { cmsSvc, adminSvc } from "@USupport-components-library/services";
 
 import "./article-information.scss";
 
@@ -23,41 +23,77 @@ export const ArticleInformation = () => {
 
   const { i18n, t } = useTranslation("article-information");
 
+  const getArticlesIds = async () => {
+    // Request articles ids from the master DB based for website platform
+    const articlesIds = await adminSvc.getArticles();
+
+    return articlesIds;
+  };
+
+  const articleIdsQuerry = useQuery(["articleIds"], getArticlesIds);
+
   const getArticleData = async () => {
-    const { data } = await cmsSvc.getArticleById(id, i18n.language);
+    let articleIdToFetch = id;
+
+    const { data } = await cmsSvc.getArticleById(
+      articleIdToFetch,
+      i18n.language
+    );
+
     const finalData = destructureArticleData(CMS_HOST, data);
     return finalData;
   };
 
-  const { data: articleData } = useQuery(["article", id], getArticleData, {
-    enabled: !!id,
-  });
+  const { data: articleData, isFetching: isFetchingArticleData } = useQuery(
+    ["article", i18n.language, id],
+    getArticleData,
+    {
+      enabled: !!id,
+    }
+  );
 
   const getSimilarArticles = async () => {
-    const { data } = await cmsSvc.getSimilarArticles(
-      3,
-      articleData.categoryId,
-      articleData.id,
-      i18n.language
-    );
+    let { data } = await cmsSvc.getArticles({
+      limit: 3,
+      categoryId: articleData.categoryId,
+      locale: i18n.language,
+      excludeId: articleData.id,
+      populate: true,
+      ids: articleIdsQuerry.data,
+    });
+
     if (data.length === 0) {
-      const { data: newest } = await cmsSvc.getNewestArticles(3, i18n.language);
+      let { data: newest } = await cmsSvc.getArticles({
+        limit: 3,
+        sortBy: "createdAt", // Sort by created date
+        sortOrder: "desc", // Sort in descending order
+        locale: i18n.language,
+        excludeId: articleData.id,
+        populate: true,
+        ids: articleIdsQuerry.data,
+      });
       return newest;
     }
     return data;
   };
 
-  const { data: moreArticles } = useQuery(
-    ["more-articles", id],
-    getSimilarArticles,
-    {
-      enabled: articleData && articleData.categoryId ? true : false,
-    }
-  );
+  const {
+    data: moreArticles,
+    isLoading: isMoreArticlesLoading,
+    isFetched: isMoreArticlesFetched,
+  } = useQuery(["more-articles", id, i18n.language], getSimilarArticles, {
+    enabled:
+      !isFetchingArticleData &&
+      !articleIdsQuerry.isLoading &&
+      articleIdsQuerry.data?.length > 0 &&
+      articleData &&
+      articleData.categoryId
+        ? true
+        : false,
+  });
 
   const onArticleClick = () => {
     window.scrollTo(0, 0);
-    setMoreArticles(null);
   };
 
   return (
@@ -78,8 +114,7 @@ export const ArticleInformation = () => {
       ) : (
         <Loading size="lg" />
       )}
-
-      {moreArticles && moreArticles.length > 0 ? (
+      {!isMoreArticlesLoading && moreArticles.length > 0 && (
         <Block classes="page__article-information__more-articles">
           <Grid classes="page__article-information__more-articles__main-grid">
             <GridItem md={8} lg={12} classes="more-articles__heading-item">
@@ -113,9 +148,15 @@ export const ArticleInformation = () => {
             })}
           </Grid>
         </Block>
-      ) : (
-        <Loading size="lg" />
       )}
+      {!moreArticles && isMoreArticlesLoading && <Loading size="lg" />}
+      {!moreArticles?.length &&
+        !isMoreArticlesLoading &&
+        isMoreArticlesFetched && (
+          <h3 className="page__article-information__no-results">
+            {t("no_results")}
+          </h3>
+        )}
     </Page>
   );
 };
