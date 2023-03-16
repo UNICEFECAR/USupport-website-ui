576a9724e6c4aeedbb13644e3df7d5220be68394
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-03-16 02:10:40 +0200

    Fix: redirect func in MeetOurProviderOverview block

--- src/blocks/MeetOurProvidersOverview/MeetOurProvidersOverview.jsx
+++ src/blocks/MeetOurProvidersOverview/MeetOurProvidersOverview.jsx
@@ -83,7 +83,7 @@ export const MeetOurProvidersOverview = () => {
             label={t("button_label")}
             size="lg"
             type="secondary"
-            onClick={() => navigate(`/about-us`)}
+            onClick={() => navigate(`/how-it-works`)}
           />
         </GridItem>
       </Grid>
