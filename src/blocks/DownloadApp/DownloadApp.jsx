9673cb17cc3a2f7abbf3da32f1ebdf41f903573f
Chavdar Shishkov <chavdar.shishkov123@gmail.com>
2023-10-13 15:32:46 +0300

    Refactor: DarkTheme issues fixes

--- src/blocks/DownloadApp/DownloadApp.jsx
+++ src/blocks/DownloadApp/DownloadApp.jsx
@@ -29,50 +29,52 @@ export const DownloadApp = () => {
   const { width } = useWindowDimensions();
 
   return (
-    <Block classes="download-app" animation="fade-up">
-      <Grid>
-        <GridItem md={5} lg={6} classes="download-app__content-item">
-          <Grid classes="download-app__content-grid">
-            <GridItem md={8} lg={12} classes="download-app__heading-item">
-              <h3>{t("heading")}</h3>
-            </GridItem>
-            <GridItem md={8} lg={12} classes="download-app__sub-heading">
-              <p className="text">{t("paragraph")}</p>
-            </GridItem>
-            <GridItem md={4} lg={6} classes="download-app__buttons-item">
-              <StoreButton
-                downloadText={t("download_text")}
-                size="lg"
-                store="google-play"
+    <div className="download-app-wrapper">
+      <Block classes="download-app" animation="fade-up">
+        <Grid>
+          <GridItem md={5} lg={6} classes="download-app__content-item">
+            <Grid classes="download-app__content-grid">
+              <GridItem md={8} lg={12} classes="download-app__heading-item">
+                <h3>{t("heading")}</h3>
+              </GridItem>
+              <GridItem md={8} lg={12} classes="download-app__sub-heading">
+                <p className="text">{t("paragraph")}</p>
+              </GridItem>
+              <GridItem md={4} lg={6} classes="download-app__buttons-item">
+                <StoreButton
+                  downloadText={t("download_text")}
+                  size="lg"
+                  store="google-play"
+                />
+              </GridItem>
+              <GridItem md={4} lg={6} classes="download-app__buttons-item">
+                <StoreButton
+                  downloadText={t("download_text")}
+                  size="lg"
+                  store="app-store"
+                />
+              </GridItem>
+            </Grid>
+          </GridItem>
+          <GridItem md={3} lg={6} classes="download-app__image-item">
+            {width < 768 ? (
+              <StaticImage
+                png={imageSrc}
+                webp={imageSrcWebp}
+                imageClasses="download-app__image"
+                alt="mobile-app"
               />
-            </GridItem>
-            <GridItem md={4} lg={6} classes="download-app__buttons-item">
-              <StoreButton
-                downloadText={t("download_text")}
-                size="lg"
-                store="app-store"
+            ) : (
+              <StaticImage
+                png={imageDesktopSrc}
+                webp={imageDesktopSrcWebp}
+                imageClasses="download-app__image"
+                alt="mobile-app"
               />
-            </GridItem>
-          </Grid>
-        </GridItem>
-        <GridItem md={3} lg={6} classes="download-app__image-item">
-          {width < 768 ? (
-            <StaticImage
-              png={imageSrc}
-              webp={imageSrcWebp}
-              imageClasses="download-app__image"
-              alt="mobile-app"
-            />
-          ) : (
-            <StaticImage
-              png={imageDesktopSrc}
-              webp={imageDesktopSrcWebp}
-              imageClasses="download-app__image"
-              alt="mobile-app"
-            />
-          )}
-        </GridItem>
-      </Grid>
-    </Block>
+            )}
+          </GridItem>
+        </Grid>
+      </Block>
+    </div>
   );
 };
