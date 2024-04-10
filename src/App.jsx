394b89402b087b56922403e7e9eae75c3ec920d0
Georgi 7DIGIT <georgi@7digit.io>
2024-04-10 20:02:54 +0300

    Add: send request to add platform access log

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import { useWebPSupportCheck } from "react-use-webp-support-check";
 import {
   Landing,
@@ -19,6 +23,7 @@ import {
   MyQA,
 } from "#pages";
 import { ThemeContext } from "@USupport-components-library/utils";
+import { userSvc } from "@USupport-components-library/services";
 
 // AOS imports
 import "aos/dist/aos.css";
@@ -59,34 +64,41 @@ function App() {
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className={`theme-${theme}`}>
         <QueryClientProvider client={queryClient}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/how-it-works" element={<HowItWorks />} />
-              <Route path="/about-us" element={<AboutUs />} />
-              <Route path="/about-us/provider" element={<ProviderOverview />} />
-              <Route path="/contact-us" element={<ContactUs />} />
-              <Route
-                path="/information-portal"
-                element={<InformationPortal />}
-              />
-              <Route
-                path="/information-portal/article/:id"
-                element={<ArticleInformation />}
-              />
-              <Route path="/my-qa" element={<MyQA />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="/sos-center" element={<SOSCenter />} />
-              <Route path="/cookie-policy" element={<CookiePolicy />} />
-              <Route path="/terms-of-use" element={<TermsOfUse />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Router>
+          <Root />
           <ReactQueryDevtools initialOpen />
         </QueryClientProvider>
       </div>
     </ThemeContext.Provider>
   );
 }
+
+const Root = () => {
+  useQuery({
+    queryKey: ["addPlatformAccess"],
+    queryFn: async () => await userSvc.addPlatformAccess("website"),
+  });
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-us/provider" element={<ProviderOverview />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/information-portal" element={<InformationPortal />} />
+        <Route
+          path="/information-portal/article/:id"
+          element={<ArticleInformation />}
+        />
+        <Route path="/my-qa" element={<MyQA />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/sos-center" element={<SOSCenter />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/terms-of-use" element={<TermsOfUse />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
