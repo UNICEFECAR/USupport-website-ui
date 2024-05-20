bbb10ecddbdd747b3b6311f4307bd93689e2db3e
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2024-05-20 13:33:02 +0300

    Add: urkainian and polish translations

--- src/pages/NotFound/locales.js
+++ src/pages/NotFound/locales.js
@@ -1,3 +1,5 @@
 export * as en from "./locales/en.json";
-export * as ru from "./locales/ru.json"
-export * as kk from "./locales/kk.json"
+export * as ru from "./locales/ru.json";
+export * as kk from "./locales/kk.json";
+export * as uk from "./locales/uk.json";
+export * as pl from "./locales/pl.json";
