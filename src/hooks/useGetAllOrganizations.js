7a60d93bfd2e8dbfdca278b7c8d9103b4403aa51
7Digit-main <it-support@7digit.io>
2025-06-30 20:08:49 +0300

    Add: custom navigation and organization hooks

---
+++ src/hooks/useGetAllOrganizations.js
@@ -0,0 +1,67 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { clientSvc } from "@USupport-components-library/services";
+
+export default function useGetAllOrganizations(filters) {
+  const {
+    search,
+    workWith,
+    district,
+    paymentMethod,
+    userInteraction,
+    specialisation,
+    userLocation,
+  } = filters;
+
+  const fetchOrganizations = async () => {
+    const { data } = await clientSvc.getOrganizations(filters);
+
+    return data.map((organization) => ({
+      organizationId: organization.organization_id,
+      name: organization.name,
+      unitName: organization?.unit_name,
+      websiteUrl: organization?.website_url,
+      address: organization?.address,
+      phone: organization?.phone,
+      email: organization?.email,
+      description: organization?.description,
+      location: {
+        longitude: organization?.longitude,
+        latitude: organization?.latitude,
+      },
+      district: {
+        id: organization?.district_id,
+        name: organization?.district,
+      },
+      paymentMethod: {
+        id: organization?.payment_method_id,
+        name: organization?.payment_method,
+      },
+      userInteraction: {
+        id: organization?.user_interaction_id,
+        name: organization?.user_interaction,
+      },
+      workWith: organization?.work_with || [],
+      providers: organization?.providers || [],
+      createdBy: organization?.created_by,
+      createdAt: organization?.created_at,
+      specialisations: organization?.specialisations || [],
+    }));
+  };
+
+  return useQuery({
+    queryKey: [
+      "organizations",
+      search,
+      workWith,
+      district,
+      paymentMethod,
+      userInteraction,
+      specialisation,
+      userLocation,
+    ],
+    queryFn: fetchOrganizations,
+  });
+}
+
+export { useGetAllOrganizations };
---
+++ src/hooks/useGetOrganizationById.js
@@ -0,0 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { clientSvc } from "@USupport-components-library/services";
+
+export default function useGetOrganizationById(organizationId) {
+  const fetchOrganization = async () => {
+    const { data } = await clientSvc.getOrganizationById(organizationId);
+    return {
+      organizationId: data.organization_id,
+      name: data.name,
+      unitName: data?.unit_name,
+      websiteUrl: data?.website_url,
+      address: data?.address,
+      phone: data?.phone,
+      email: data?.email,
+      description: data?.description,
+      location: {
+        longitude: data?.longitude,
+        latitude: data?.latitude,
+      },
+      district: {
+        id: data?.district_id,
+        name: data?.district,
+      },
+      paymentMethod: {
+        id: data?.payment_method_id,
+        name: data?.payment_method,
+      },
+      userInteraction: {
+        id: data?.user_interaction_id,
+        name: data?.user_interaction,
+      },
+      workWith: data?.work_with || [],
+      providers: data?.providers || [],
+      createdBy: data?.created_by,
+      createdAt: data?.created_at,
+      specialisations: data?.specialisations || [],
+    };
+  };
+
+  return useQuery({
+    queryKey: ["organization", organizationId],
+    queryFn: fetchOrganization,
+    enabled: !!organizationId,
+  });
+}
+
+export { useGetOrganizationById };
