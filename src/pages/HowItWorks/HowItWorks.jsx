00e9bd9af8e6db7cd3a411a11af99c0d42927a45
7Digit-main <it-support@7digit.io>
2025-11-13 14:07:56 +0200

    Refactor: Hide the MeetOurProviders block from the HowItWorks page for Romania

--- src/pages/HowItWorks/HowItWorks.jsx
+++ src/pages/HowItWorks/HowItWorks.jsx
@@ -19,6 +19,7 @@ import "./how-it-works.scss";
  */
 export const HowItWorks = () => {
   const faqRef = useRef();
+  const IS_RO = localStorage.getItem("country") === "RO";
 
   useEffect(() => {
     const to = new URLSearchParams(window.location.search).get("to");
@@ -32,7 +33,7 @@ export const HowItWorks = () => {
   return (
     <Page classes="page__how-it-works">
       <HowItWorksBlock showSummaryBellow={true} onPage={true} />
-      <MeetOurProviders />
+      {IS_RO ? <MeetOurProviders /> : null}
       <VideoTutorial />
       <div ref={faqRef} />
       <FAQ showLearnMore={false} showMascot={false} />
