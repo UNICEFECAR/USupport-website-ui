65600c0cca099d9e0de1497c0eaba90278278096
7Digit-Main <it-support@7digit.io>
2026-01-19 14:27:47 +0200

    Fix: generateSitemap

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -10,46 +10,43 @@ export function generateSitemap({
 }) {
     const lastmod = new Date().toISOString().split("T")[0]; // YYYY-MM-DD
 
-  const urlsetOpen = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-  xmlns:xhtml="http://www.w3.org/1999/xhtml"
->
-`;
-
-  const urlsetClose = `</urlset>`;
-
-  const urls = routes
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="http://www.w3.org/1999/xhtml"
+  >
+  ${routes
     .map((route) => {
       const priority = route === "/" ? "1.0" : "0.8";
-      const defaultUrl = `${siteUrl}/${defaultLang}${route}`;
-
+      const pathPart = route === "/" ? "" : route;
+      const defaultUrl = `${siteUrl}/${defaultLang}${pathPart}`;
+  
       const alternates = languages
         .map(
           (lang) => `
-    <xhtml:link
-      rel="alternate"
-      hreflang="${lang}"
-      href="${siteUrl}/${lang}${route}" />`
+      <xhtml:link
+        rel="alternate"
+        hreflang="${lang}"
+        href="${siteUrl}/${lang}${pathPart}" />`
         )
         .join("");
-
+  
       return `
-  <url>
-    <loc>${defaultUrl}</loc>${alternates}
-    <lastmod>${lastmod}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>${priority}</priority>${alternates}
-
-    <xhtml:link
-      rel="alternate"
-      hreflang="x-default"
-      href="${defaultUrl}" />
-  </url>`;
+    <url>
+      <loc>${defaultUrl}</loc>
+  ${alternates}
+      <xhtml:link
+        rel="alternate"
+        hreflang="x-default"
+        href="${defaultUrl}" />
+      <lastmod>${lastmod}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>${priority}</priority>
+    </url>`;
     })
-    .join("");
+    .join("")}
+  </urlset>`;
 
-  const sitemap = urlsetOpen + urls + "\n" + urlsetClose;
 
   fs.writeFileSync(path.join(outDir, "sitemap.xml"), sitemap, "utf8");
 
